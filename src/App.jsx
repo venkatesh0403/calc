ac735cb76da6d5d1c1f3a3584bc4a2a6eb9f6e51
Venkatesh.S <venkateshkaviya1234@gmail.com>
2024-12-13 09:47:51 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calc from './components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calc />
+    </div>
+  );
+}
+
+export default App;
